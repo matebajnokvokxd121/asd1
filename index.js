8b532f0accf3ed490201ea864cd884019ecf846c
matebajnokvokxd121 <110678927+matebajnokvokxd121@users.noreply.github.com>
2022-08-05 16:42:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const Discord = require("discord.js");
-const tokenfile = require("./tokenfile.json");
 const botconfig = require("./botconfig.json");
 const bot = new Discord.Client({disableEveryone: true});
 
@@ -118,4 +117,4 @@ bot.on("message", async message => {
 
 
 
-bot.login(tokenfile.token);
+bot.login(process.env.BOT_TOKEN);
